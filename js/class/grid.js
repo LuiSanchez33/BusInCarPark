dbefa103212938e1e522ee2f53f78f70d4fc165a
Luis Sanchez <34088502+LuiSanchez33@users.noreply.github.com>
2018-02-11 11:45:34 +1300

    Add files via upload

--- js/class/grid.js
+++ js/class/grid.js
@@ -7,11 +7,23 @@ function grid(section, rows, cols, width, height) {
 
   this.init = function () {
     drawGrid();
+    drawCells();
   }
 
   function drawGrid() {
     this.svg = d3.select(section).append("svg")
       .attr({ width: width * cols, height: height * rows });
+  }
+
+  function drawCells() {
+    for (var y = 0; y < rows; y++) {
+      for (var x = 0; x < cols; x++) {
+        console.log(x, y);
+        svg.append("rect")
+          .attr({ "x": x * width, "y": y * height, "width": width, "height": height })
+          .style({ "fill": "green", "stroke": "white", "stroke-width": "1", "opacity": "0.2" });
+      }
+    }
   }
 
 }
